ec0e02e0d84108570e619a514273bd344bedb6f9
greentea777 <117492457+greentea777@users.noreply.github.com>
2023-07-04 17:24:40 -0700

    added menu button animation and JQuery script tag

---
+++ index.js
@@ -0,0 +1,13 @@
+$(".site-header__menu-btn").on("click", () => {
+  $(".site-header__menu-btn").toggleClass("selected");
+
+  if ($(".site-header__menu-btn").hasClass("selected")) {
+    $(".site-header__menu-btn span:nth-child(1)").text("E");
+    $(".site-header__menu-btn span:nth-child(2)").text("I");
+    $(".site-header__menu-btn span:nth-child(3)").text("T");
+  } else {
+    $(".site-header__menu-btn span:nth-child(1)").text("M");
+    $(".site-header__menu-btn span:nth-child(2)").text("N");
+    $(".site-header__menu-btn span:nth-child(3)").text("U");
+  }
+});
